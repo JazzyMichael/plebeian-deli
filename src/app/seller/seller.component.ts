7faaf6d33760ddb45259ad0d296b48a9a8f964e9
Michael Mancini <thejazzymichael@gmail.com>
2020-02-13 18:36:28 -0500

    seller order message and post formatting

--- src/app/seller/seller.component.ts
+++ src/app/seller/seller.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef, AfterViewChecked } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from '../services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-seller',
   templateUrl: './seller.component.html',
   styleUrls: ['./seller.component.scss']
 })
-export class SellerComponent implements OnInit {
+export class SellerComponent implements OnInit, AfterViewChecked, OnDestroy {
   connectBaseUrl = 'https://connect.stripe.com/oauth/authorize?';
   responseType = 'response_type=code';
   clientId = '&client_id=ca_Fh9rZqqdVbgofzaWlXpPHyNcrno2iXnY';
   // clientId = '&client_id=ca_Fh9rJPQapCoKCcWgClCGJtY6pBesglFu';
   scope = '&scope=read_write';
 
+  userSub: Subscription;
   user: any;
-  approvedSeller: boolean = false;
+  approvedSeller: boolean;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private snackBar: MatSnackBar,
+    private cd: ChangeDetectorRef
+  ) { }
 
   async ngOnInit() {
-    this.user = await this.authService.getCurrentUser();
+    this.userSub = this.authService.user$.asObservable().subscribe(user => {
+      this.user = user;
+      this.approvedSeller = this.user ? true : false;
+    });
+  }
 
-    // this.approvedSeller = this.user && this.user.approvedSeller;
+  ngAfterViewChecked(){
+    this.cd.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
   connectClick() {
@@ -29,8 +47,9 @@ export class SellerComponent implements OnInit {
     window.open(url);
   }
 
-  saveOrderMessage(msg: string) {
-    console.log(msg);
+  async saveOrderMessage(orderPlacedMessage: string) {
+    await this.userService.updateUserPromise(this.user.uid, { orderPlacedMessage });
+    this.snackBar.open('Message Updated!', '', { duration: 2000 });
   }
 
 }
